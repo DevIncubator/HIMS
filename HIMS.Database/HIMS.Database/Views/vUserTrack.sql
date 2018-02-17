baa336179988ce61758c9db4bc550b4d85a9375a
vyshka <egorvyshka@gmail.com>
2018-02-17 13:13:38 +0300

    add view

---
+++ HIMS.Database/HIMS.Database/Views/vUserTrack.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vUserTrack]
+	AS SELECT 
+		[UserProfile].[UserId],
+		[Task].[TaskId],
+		[Task].[Name],
+		[TaskTrack].[TaskTrackId],
+		[TaskTrack].[TrackNote],
+		[TaskTrack].[TrackDate]
+	 FROM [Task], [TaskTrack], [UserProfile]
