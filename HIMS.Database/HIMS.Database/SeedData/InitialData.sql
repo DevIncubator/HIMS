eaf3b837fb178802d2c5b53e601921cfe90046f1
vyshka <egorvyshka@gmail.com>
2018-02-17 13:17:12 +0300

    Add 3 constant values to Direction table

--- HIMS.Database/HIMS.Database/SeedData/InitialData.sql
+++ HIMS.Database/HIMS.Database/SeedData/InitialData.sql
@@ -4,16 +4,16 @@
 		('Sample Record #1', 'This is a simple test record for testing purposes.'),
 		('Sample Record #2', 'This is a simple test record for testing purposes too.');
 
-INSERT INTO [dbo].[Direction]
-		([Name], [Description])
-	VALUES
-		('.NET', 'The .NET direction.'),
-		('Java', 'The Java direction'),
-		('Front-End', 'The Front-End direction.');
-
 INSERT INTO [dbo].[TaskState]
 			([StateName])
 	   VALUES
 			('Active'),
 			('Success'),
 			('Fail')
+
+INSERT INTO [dbo].[Direction]
+		([Name], [Description])
+	VALUES
+		('.NET', 'The .NET direction.'),
+		('Java', 'The Java direction'),
+		('Front-End', 'The Front-End direction.');
