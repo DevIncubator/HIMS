8fd85d573e19550d19c923fcf375ecf229ff2043
vyshka <egorvyshka@gmail.com>
2018-02-18 13:04:25 +0300

    add identity to primary key

--- HIMS.Database/HIMS.Database/Tables/Direction.sql
+++ HIMS.Database/HIMS.Database/Tables/Direction.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Direction]
 (
-	[DirectionId] INT NOT NULL PRIMARY KEY, 
+	[DirectionId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(MAX) NOT NULL, 
     [Description] NVARCHAR(MAX) NOT NULL 
 )
+
+GO
