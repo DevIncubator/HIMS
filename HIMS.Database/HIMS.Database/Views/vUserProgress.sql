6ea4eb6d96a2041247e9ebed4e4157c04bb98918
Jagailo <Jagailo96@gmail.com>
2018-02-20 23:48:29 +0300

    Added UserTask table. Added vUserProgress view. (Completed 1st week task)

---
+++ HIMS.Database/HIMS.Database/Views/vUserProgress.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vUserProgress]
+	AS SELECT ISNULL(ut.UserId, -999) AS UserId,
+              ISNULL(ut.TaskId, -999) AS TaskId,
+              ISNULL(tt.TaskTrackId, -999) AS TaskTrackId,
+              up.Name AS UserName,
+              t.Name AS TaskName,
+              tt.TrackNote AS TrackNote,
+              tt.TrackDate AS TrackDate
+	FROM [UserTask] ut JOIN [TaskTrack] tt ON ut.UserTaskId = tt.UserTaskId
+	                   JOIN [UserProfile] up ON ut.UserId = up.UserId
+	                   JOIN [Task] t ON ut.TaskId = t.TaskId
