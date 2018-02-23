c233823ae46627230944dd0826fb27dd5bd3826b
vyshka <egorvyshka@gmail.com>
2018-02-23 22:38:57 +0300

    add taskTrack interface

---
+++ HIMS/HIMS.BusinessLogic/Interfaces/ITaskTrack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HIMS.BusinessLogic.DTO;
+using HIMS.Data.EntityClasses;
+
+namespace HIMS.BusinessLogic.Interfaces
+{
+    public interface ITaskTrack
+    {
+        IEnumerable<TaskTrackTransferModel> GetTaskTracks(int UserId);
+        void SaveTaskTrack(TaskTrackTransferModel item);
+        void UpdateTaskTrack(TaskTrackTransferModel item);
+        void DeleteTaskTrack(int? id);
+        TaskTrackTransferModel GetTaskTrack(int? id);
+    }
+}
