d25a56de2b825970a721e18ff27b646a65b04666
vyshka <egorvyshka@gmail.com>
2018-02-24 21:39:08 +0300

    add dependence

--- HIMS/HIMS.WebMVC/Utils/DependencesModule.cs
+++ HIMS/HIMS.WebMVC/Utils/DependencesModule.cs
@@ -14,6 +14,7 @@ namespace HIMS.WebMVC.Utils
             Bind<IVUserProfileService>().To<VUserProfileService>();
             Bind<IUserProfileService>().To<UserProfileService>();
             Bind<IDirectionService>().To<DirectionService>();
+            Bind<ITaskTrackService>().To<TaskTrackService>();
         }
     }
 }
