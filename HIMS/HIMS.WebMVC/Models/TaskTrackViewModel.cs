a2399be693f0b50a3c595edcfbf10771a6026d58
vyshka <egorvyshka@gmail.com>
2018-02-27 00:10:04 +0300

    add TaskTrack models

---
+++ HIMS/HIMS.WebMVC/Models/TaskTrackViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIMS.WebMVC.Models
+{
+    public class TaskTrackViewModel
+    {
+        public int TaskTrackId { get; set; }
+        public DateTime TrackDate { get; set; }
+        public string TrackNote { get; set; }
+
+    }
+}
