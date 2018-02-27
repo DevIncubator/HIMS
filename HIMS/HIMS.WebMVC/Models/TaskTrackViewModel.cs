e8822faabd861b7a0351c607080da3e76475e2ad
vyshka <egorvyshka@gmail.com>
2018-02-27 14:53:10 +0300

    update model

--- HIMS/HIMS.WebMVC/Models/TaskTrackViewModel.cs
+++ HIMS/HIMS.WebMVC/Models/TaskTrackViewModel.cs
@@ -10,6 +10,8 @@ namespace HIMS.WebMVC.Models
         public int TaskTrackId { get; set; }
         public DateTime TrackDate { get; set; }
         public string TrackNote { get; set; }
+        public string TaskName { get; set; }
+        public int UserTaskId { get; set; }
 
     }
 }
