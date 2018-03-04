fe4fb0d4e402fd5974937eae4543b64583b5e3df
Alexey Yagelo <Jagailo96@gmail.com>
2018-03-04 20:54:07 +0300

    Added VUserProgressTransferModel. (Completed 3rd week task)

---
+++ HIMS/HIMS.BusinessLogic/DTO/VUserProgressTransferModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HIMS.BusinessLogic.DTO
+{
+    public class VUserProgressTransferModel
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TrackNote { get; set; }
+        public DateTime TrackDate { get; set; }
+    }
+}
