4b65b351315168613369073cbad61c2b4f9de474
SergeiOrlovski <orlovskiy_1990@bk.ru>
2018-03-07 15:42:10 +0300

    Add UserTaskTransferModel

--- HIMS/HIMS.BusinessLogic/DTO/UserTaskTransferModel.cs
+++ HIMS/HIMS.BusinessLogic/DTO/UserTaskTransferModel.cs
@@ -9,8 +9,8 @@ namespace HIMS.BusinessLogic.DTO
 {
     public class UserTaskTransferModel
     {
-        public int userId { get; set; }
-        public int taskId { get; set; }
+        public int UserId { get; set; }
+        public int TaskId { get; set; }
         public string Name { get; set; }
         public DateTime Start { get; set; }
         public DateTime Deadline { get; set; }
