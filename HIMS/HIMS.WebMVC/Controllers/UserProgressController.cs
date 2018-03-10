3d6cde5070287b05a760ec22d44422e02c2a01c6
Alexey Yagelo <Jagailo96@gmail.com>
2018-03-10 17:28:55 +0300

    Completed 4th and 5th week task.

--- HIMS/HIMS.WebMVC/Controllers/UserProgressController.cs
+++ HIMS/HIMS.WebMVC/Controllers/UserProgressController.cs
@@ -10,43 +10,26 @@ namespace HIMS.WebMVC.Controllers
     public class UserProgressController : Controller
     {
         private readonly IVUserProgressService _vUserProgressService;
-        private readonly IVUserProfileService _vUserProfileService;
 
-        public UserProgressController(IVUserProgressService vUserProgressService, IVUserProfileService vUserProfileService)
+        public UserProgressController(IVUserProgressService vUserProgressService)
         {
             _vUserProgressService = vUserProgressService;
-            _vUserProfileService = vUserProfileService;
         }
 
-        [HttpPost]
+        [Authorize(Roles = "admin")]
         public ActionResult Index(int? userId)
         {
-            IEnumerable<VUserProgressTransferModel> userProgressDTO = null;
-            string userName = "";
+            UserProgressListViewModel userProgress = new UserProgressListViewModel();
 
             if (userId.HasValue)
             {
-                userProgressDTO = _vUserProgressService.GetProgressByUserId(userId.Value);
-                userName = _vUserProgressService.GetUserNameById(userId.Value);
-            }
-            else
-            {
-                var userIdentityName = User.Identity.Name;
-                var user = _vUserProfileService.GetVUserProfile(userIdentityName);
-                if (user != null)
-                {
-                    userProgressDTO = _vUserProgressService.GetProgressByUserId(user.UserId);
-                    userName = _vUserProgressService.GetUserNameById(user.UserId);
-                }
+                IEnumerable<VUserProgressTransferModel> userProgressDTO = _vUserProgressService.GetProgressByUserId(userId.Value);
+                string userName = _vUserProgressService.GetUserNameById(userId.Value);
+
+                userProgress.UserProgressList = Mapper.Map<IEnumerable<VUserProgressTransferModel>, List<UserProgressViewModel>>(userProgressDTO);
+                userProgress.UserName = userName;
             }
 
-            var userProgress = new UserProgressListViewModel
-            {
-                UserProgressList = Mapper.Map<IEnumerable<VUserProgressTransferModel>, List<UserProgressViewModel>>(userProgressDTO),
-                UserName = userName
-            };
-
-            ViewBag.Title = $"{userName}'s progress";
             return View(userProgress);
         }
     }
