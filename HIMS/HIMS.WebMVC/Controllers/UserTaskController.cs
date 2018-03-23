c73783b2da61a60a33c9e4a8a2ed86184fa11e50
SergeiOrlovski <orlovskiy_1990@bk.ru>
2018-03-23 15:39:17 +0300

    Create controller and Model

---
+++ HIMS/HIMS.WebMVC/Controllers/UserTaskController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using HIMS.BusinessLogic.DTO;
+using HIMS.BusinessLogic.Interfaces;
+using HIMS.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HIMS.WebMVC.Controllers
+{
+    public class UserTaskController : Controller
+    {
+        private readonly IUserTaskService _service;
+
+        public UserTaskController(IUserTaskService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult GetTasksForUser(int id)
+        {
+            if (id != null)
+            {
+                IEnumerable<UserTaskTransferModel> userDtos = _service.GetAllTasksForUser(id);
+                var tasks = new UserTasksListViewModel
+                {
+                    UserTasksList = Mapper.Map<IEnumerable<UserTaskTransferModel>, List<UserTaskViewModel>>(userDtos)
+                };
+                return View(tasks);
+
+            }
+            else { return new HttpStatusCodeResult(HttpStatusCode.BadRequest); }
+            
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public ActionResult SetTaskAsSuccess(int userId, int taskId, bool isSuccess)
+        {
+            _service.UpdateTaskStatusForUser(userId, taskId, isSuccess);
+            var tasksDTO = _service.GetAllTasksForUser(userId).ToList();
+            var tasks = Mapper.Map<IEnumerable<UserTaskTransferModel>, List<UserTaskViewModel>>(tasksDTO);
+            return PartialView("_GetTasksForUser", tasks);
+        }
+       
+    }
+}
---
+++ HIMS/HIMS.WebMVC/Models/UserTasksListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIMS.WebMVC.Models
+{
+    public class UserTasksListViewModel
+    {
+        public IEnumerable<UserTaskViewModel> UserTasksList { get; set; }
+    }
+}
