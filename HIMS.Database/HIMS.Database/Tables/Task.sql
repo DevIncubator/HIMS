494318219b4c63493b8a7db057593527512aee1d
LizaAgeeva <lizaageeva1996@mail.ru>
2018-04-09 23:43:02 +0300

    Editing Task table

--- HIMS.Database/HIMS.Database/Tables/Task.sql
+++ HIMS.Database/HIMS.Database/Tables/Task.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Task]
 (
-	[TaskId] INT NOT NULL PRIMARY KEY, 
+	[TaskId] INT IDENTITY(1,1) NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Description] NVARCHAR(255) NOT NULL, 
     [StartDate] DATE NOT NULL, 
     [DeadlineDate] DATE NOT NULL
+
+	CONSTRAINT [PK_Task] PRIMARY KEY ([TaskId])
 )
